256d70e5a1e92c959a9c0dd80aa9e79c75570f65
Anton Changalidi <anton.chana@gmail.com>
2021-03-18 10:55:20 +0300

    distribution by files done

---
+++ genius_info.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <blkid/blkid.h>
+
+#define ll long long
+#define TB_amount pow(2, 40)
+#define GB_amount pow(2, 30)
+#define MB_amount pow(2, 20)
+#define KB_amount pow(2, 10)
+
+
+void print_size(ll size);
+
+int initialize_cache(blkid_cache *cache);
+
+int probe_cache(blkid_cache *cache);
+
+void check_and_print(blkid_probe *probe, char *tag);
+
+int iterate_dev(blkid_cache *cache);
---
+++ main.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <blkid/blkid.h>
+#include "genius_info.h"
+#include "genius_ntfs.h"
